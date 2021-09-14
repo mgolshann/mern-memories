4efe807f59c793851a3d98102b2464774857d182
mgolshann <m_golshan85@yahoo.com>
2021-09-14 00:07:55 -0700

    creating foleder structure and style the posts component and form component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Container, Grow, Grid, AppBar, Typography} from '@material-ui/core';
+
+// Components
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import memories from './images/memories.png'
+
+// Styles
+import useStyle from './styles'
 
 const App = () => {
+    const classes = useStyle();
     return (
-        <div>App</div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch">
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
